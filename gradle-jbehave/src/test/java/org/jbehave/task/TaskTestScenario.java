eb171ef8becd48341fb4060048b86efae68ce216
Sergei Chipiga <schipiga@mirantis.com>
2017-02-07 18:03:23 +0200

    Add jbehave proto

---
+++ gradle-jbehave/src/test/java/org/jbehave/task/TaskTestScenario.java
@@ -0,0 +1,89 @@
+package org.jbehave.task;
+
+import org.jbehave.core.annotations.BeforeStories;
+import org.jbehave.core.annotations.Given;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import static org.jbehave.task.support.BehaviouralTestEmbedder.aBehaviouralTestRunner;
+import static com.codeborne.selenide.Selenide.*;
+import static org.assertj.core.api.Assertions.*;
+
+/**
+ * Created by schipiga on 07.02.17.
+ */
+public class TaskTestScenario {
+
+    @Test
+    public void berlinClockAcceptanceTests() throws Exception {
+        aBehaviouralTestRunner()
+                .usingStepsFrom(this)
+                .withStory("task.story")
+                .run();
+    }
+
+    @BeforeStories
+    public void beforeStories() {
+        System.setProperty("webdriver.chrome.driver", "/home/schipiga/vega/projects/google-test/chromedriver");
+        System.setProperty("selenide.browser", "chrome");
+    }
+
+    @Given("I open a browser")
+    public void givenIOpenABrowser() {
+        // PENDING
+    }
+
+    @Given("I go to $url")
+    public void givenIGoTo(String url) {
+        open(url);
+    }
+
+    @Given("I change language to $lang")
+    public void givenIChangeLanguageTo(String lang) {
+        $(By.linkText("RU")).click();
+    }
+
+    @Given("I search $query with min cost $minCost with max cost $maxCost with region $region with period $period")
+    public void givenISearch(String query, String minCost, String maxCost, String region, String period) {
+        $(By.name("topt[8][min]")).setValue(minCost);
+        $(By.name("topt[8][max]")).setValue(maxCost);
+
+        $(By.name("search_region")).selectOptionContainingText(region);
+        $(By.name("pr")).selectOptionContainingText(period);
+        $(By.name("txt")).setValue(query);
+
+        $(By.name("ffrm")).submit();
+    }
+
+    @Given("I sort results by $sortType")
+    public void givenISortBy(String sortType) {
+        $(By.linkText(sortType)).click();
+    }
+
+    @Given("I select deal type as $dealType")
+    public void givenISelectDealTypeAs(String dealType) {
+        $(By.xpath("//span[text()='Тип сделки:']//select")).selectOptionContainingText(dealType);
+    }
+
+    @Given("I click section $section")
+    public void givenIClickSection(String section) {
+        $(By.linkText(section)).click();
+    }
+
+    @Given("I open search page")
+    public void givenIOpenSearchPage() {
+        $(By.linkText("Поиск")).click();
+    }
+
+    @Given("I open expanded search")
+    public void givenIOpenExpandedSearch() {
+        $(By.linkText("Расширенный поиск")).click();
+    }
+
+    @Given("I search with min cost $minCost with max cost $maxCost")
+    public void givenIExpandSearch(String minCost, String maxCost) {
+        $(By.name("topt[8][min]")).setValue(minCost);
+        $(By.name("topt[8][max]")).setValue(maxCost);
+        $(By.name("ffrm")).submit();
+    }
+}
---
+++ gradle-jbehave/src/test/resources/stories/task.story
@@ -0,0 +1,18 @@
+Meta:
+
+Narrative:
+As a user
+I want to perform an action
+So that I can achieve a business goal
+
+Scenario: scenario description
+Given I open a browser
+And I go to http://ss.lv
+And I change language to RU
+And I click section Электротехника
+And I open search page
+And I search Компьютер with min cost 100 with max cost 1000 with region Рига with period За последний месяц
+And I sort results by Цена
+And I select deal type as Продажа
+And I open expanded search
+And I search with min cost 0 with max cost 300
